571fdc5133a5fd969faa5bd915934ad27df85957
dat <datdatdat1348@gmail.com>
2022-07-07 16:27:11 +0700

    INT

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,10 @@ fun main() {
     val interestingThings = arrayOf("kotlin", "dat", "thanh")
     interestingThings.dwada
 <<<<<<< HEAD
+<<<<<<< HEAD
     dwuaiduwadihwa
 =======
 >>>>>>> ef57665 (UI)
+=======
+    dwuaiduwadihwa
+>>>>>>> 3a778d3 (INT)
