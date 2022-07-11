d1c0e11f0ccfd83e666b6c7bb1b0ab9ee1259d0b
dat <datdatdat1348@gmail.com>
2022-07-11 16:29:04 +0700

    asdasd

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,6 @@
-fun sayHello(greeting: String, vararg itemToGreet: String){
-    itemToGreet.forEach { itemToGreet->
-        println("$greeting $itemToGreet")
-    }
-}
-fun greetPerson(greeting: String = "thanh", name : String = "dat") = println("$greeting $name")
-fun main()  {
-   /*val interestingThings = arrayOf("ta", "thanh", "dat")
-    sayHello(greeting = "hi", itemToGreet = *interestingThings)*/
-    val person = Person()
+fun sayHello(greeting: String, itemToGreet: String) = println("$greeting $itemToGreet")
 
-    person.printInfo()
-
-}
+fun main() {
+    val interestingThings = arrayOf("kotlin", "dat", "thanh")
+    interestingThings.dwada
+    dwuaiduwadihwa
